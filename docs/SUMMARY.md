5c6d98aa5fe30fb3703b350ba278cdfe4fe0b32d
alu0100894325 <alu0100894325@ull.edu.es>
2017-03-14 10:47:05 +0000

    Closes #10 - Añadidos apuntes práctica rutas

---
+++ docs/SUMMARY.md
@@ -0,0 +1,10 @@
+# Capitulos
+
+* [Estudiar rutas en express](rutas.md)
+    * [Routing Básico](rutas/chapter1.md)
+    * [Routing General](rutas/chapter2.md)
+    * [Middleware](rutas/chapter3.md)
+    * [Router API](rutas/chapter4.md)
+    * [Links](links.md)
+*  [Estudiar cookies y sessions](cookies.md)
+    * [Links](cookies/links.md)
---
+++ src/gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('default', function() {
+
+});
+
+gulp.task('ejemploRutas', function() {
+  var exec = require('child_process').exec;
+    var child;
+    child = exec("node ejemploRutas.js", 
+                function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+    });
+});
+
+gulp.task('ejemploAPI', function() {
+  var exec = require('child_process').exec;
+    var child;
+    child = exec("node ejemploAPI.js", 
+                function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+    });
+});
+
+gulp.task('ejemploExpress', function() {
+  var exec = require('child_process').exec;
+    var child;
+    child = exec("node ejemploExpress.js", 
+                function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+    });
+});
