b44f932e6400395982fdb333bc991cbba9370717
alu0100821215 <alu0100821215@ull.edu.es>
2017-03-15 15:59:19 +0000

    Añadido código ejemplo en src

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -5,6 +5,19 @@ gulp.task('default', function() {
 
 });
 
+gulp.task('ejemploCookies', function() {
+  var exec = require('child_process').exec;
+    var child;
+    child = exec("node EjemploCookies.js", 
+                function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+    });
+});
+
 gulp.task('ejemploRutas', function() {
   var exec = require('child_process').exec;
     var child;
